f259964ebbce523208be46e4a5d4259950d52de7
iwanbazz <iwan.bazz@gmail.com>
2022-12-13 21:25:21 +0700

    fix: change user agent

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -3,7 +3,7 @@ CSV.foreach("./seeder/asins.csv", headers: true) do |row|
   pages << {
     page_type: "products",
     method: "GET",
-    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"},
     url: url,
     force_fetch: true,
     vars: {
